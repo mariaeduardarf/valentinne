c7709b18018c1e94dc217e42ddc4882ef221fdea
Maria Eduarda <maria.rf.veleiros@gmail.com>
2025-04-03 15:15:14 -0300

    update menu js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+(function () {
+
+    "use strict";
+  /*O modo estrito ("use strict") é ativado para ajudar a detectar erros e comportamentos problemáticos no código. */
+
+  /**
+   * Função de seleção
+   */
+  
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function 
+   * Função de Adição de Eventos (on)
+   * Função espera que um evento ocorra e depois responde a ele  */
+  
+  
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  // Ativar o botão menu no modo mobile 
+
+  on('click', '.mobile-nav-toggle', function() {
+    select('body').classList.toggle('mobile-nav-active') 
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+ })()
