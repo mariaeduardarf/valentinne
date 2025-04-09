d0423dd12f8065a7a445abcd4117a1b0f829b9b1
Maria Eduarda <maria.rf.veleiros@gmail.com>
2025-04-09 16:06:50 -0300

    update hero

--- js/script.js
+++ js/script.js
@@ -41,4 +41,16 @@
     this.classList.toggle('bi-x')
   })
 
+  // Efeito Digitação
+  const typed = select('.typed');
+
+  if (typed) {
+    let typed_strings = typed.getAttribute ('data-typed-items')
+    typed_strings = typed_strings.split(',')
+
+    new Typed('.typed', {
+      strings: typed_strings,
+    })
+  }
+
  })()
